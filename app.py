a4e016c4bfecf932fa647810de727d7b66075633
Luis-Jesus-AM <23308060610060@cetis61.edu.mx>
2025-11-08 22:06:53 -0700

    sabado

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from urllib import request
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
+
+usuarios = []
+
 
 @app.route("/")
 def index():
@@ -10,6 +15,11 @@ def index():
 def nutrien():
     return render_template("nutrien.html")
 
+@app.route("/registros")
+def registros():
+    return render_template("registros.html")
+
+
 @app.route("/comida")
 def comida():
     return render_template("comida.html")
@@ -18,9 +28,23 @@ def comida():
 def ingre():
     return render_template("ingre.html")
 
-@app.route("/iniciar")
-def iniciar():
-    return render_template("iniciar.html")
+@app.route("/registrar", methods=["POST"])
+def registrar():
+    nombre = request.form["nombre"]
+    email = request.form["email"]
+    contraseña = request.form["contraseña"]
+
+    usuario = {
+        "nombre": nombre,
+        "email": email,
+        "contraseña": contraseña
+    }
+
+    usuarios.append(usuario)
+    print(usuarios)
+
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
