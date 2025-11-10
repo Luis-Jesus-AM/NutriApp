fd17d4934295842019236c0bce2d1788b009dd5b
Luis-Jesus-AM <23308060610060@cetis61.edu.mx>
2025-11-09 17:59:49 -0700

    domingo

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from urllib import request
-from flask import Flask, render_template, request, redirect, url_for
-
-
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
-usuarios = []
+app.secret_key = "clave_super_secreta"
 
+def login_requerido(ruta):
+    def wrapper(*args, **kwargs):
+        if "usuario" not in session:
+            return redirect(url_for("sesion"))
+        return ruta(*args, **kwargs)
+    wrapper.__name__ = ruta.__name__
+    return wrapper
+
+usuarios = [
+    {
+        "nombre": "luis",
+        "email": "23308060610060@cetis61.edu.mx",
+        "contraseña": "123456"
+    }
+]
 
 @app.route("/")
 def index():
@@ -15,18 +27,43 @@ def index():
 def nutrien():
     return render_template("nutrien.html")
 
+@app.route("/sesion")
+def sesion():
+    return render_template("sesion.html")
+
+@app.route("/iniciar-sesion", methods=["POST"])
+def iniciar_sesion():
+    email = request.form["email"]
+    contraseña = request.form["contraseña"]
+
+    for u in usuarios:
+        if u["email"] == email and u["contraseña"] == contraseña:
+            session["usuario"] = email   
+            return redirect(url_for("index"))
+
+    return "Usuario o contraseña incorrectos"
+
+
+
 @app.route("/registros")
 def registros():
     return render_template("registros.html")
 
-
 @app.route("/comida")
+@login_requerido
 def comida():
     return render_template("comida.html")
 
 @app.route("/ingre")
+@login_requerido
 def ingre():
     return render_template("ingre.html")
+
+@app.route("/cerrar-sesion")
+def cerrar_sesion():
+    session.pop("usuario", None)
+    return redirect(url_for("index"))
+
 
 @app.route("/registrar", methods=["POST"])
 def registrar():
@@ -43,8 +80,7 @@ def registrar():
     usuarios.append(usuario)
     print(usuarios)
 
-    return redirect(url_for('index'))
-
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
